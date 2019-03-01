4256e607106ddf96e31ccc219d924b1dc97d45aa
saroj <kandelsoftware@gmail.com>
2019-03-01 23:19:33 +0100

    update order state

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,48 @@
 import React from 'react';
-import Header from './Header';
-import Inventory from './Inventory';
-import Order from './Order';
+import Header from './container/Header';
+import Inventory from './container/Inventory';
+import Order from './container/Order';
+import Fish from './container/Fish';
+
+import sampleFishes from '../sample-fishes'
 class App extends React.Component {
+
+    state = {
+        fishes: {},
+        order: {}
+    };
+    addFish = (fish) => {
+        const fishes = { ...this.state.fishes }
+        fishes[`fish${Date.now()}`] = fish;
+
+        this.setState({
+            fishes
+        })
+    };
+    loadSampleFishes = () => {
+        this.setState({
+            fishes: sampleFishes,
+        });
+
+    }
+
     render() {
         return (
             <div className="catch-of-the-day" >
-                <div className="menu">
+                <div className="menu" >
                     <Header tagline="Fresh seafood Market" />
-                </div>
-                <div> <Inventory />
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key => (<Fish key={key} details={this.state.fishes[key]} />
+                        ))}
+                    </ul>
                 </div>
                 <Order />
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
+
             </div >
 
         );
     };
-}
+};
 
 export default App;
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,18 +1,28 @@
 import React from 'react';
-
+import { getFunName } from "../helpers";
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+    goToStore = event => {
+        event.preventDefault();
+        const storeName = (this.myInput.current.value);
+        this.props.history.push(`/store/${storeName}`);
 
+    }
     render() {
-        return (
-            <form className="store-selector">
-                <input type="text" required placeholder="Enter store " />
-                <button type="summit">visit store</button>
-            </form>
 
+        return (
+            <form className="store-selector" onSubmit={this.goToStore}>
+                <input
+                    type="text"
+                    ref={this.myInput}
+                    required
+                    placeholder="Enter store "
+                    defaultValue={getFunName()}
+                />
+                <button type="submit">visit store</button>
+            </form>
         );
     }
-
-
 }
 
-export default StorePicker
+export default StorePicker;
